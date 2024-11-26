184ce7bffddcfbee6e62c0ca410aa7c3b8818567
Gmatieso <matiesogeoffrey@gmail.com>
2024-11-26 16:17:41 +0300

    Task model done

--- src/main/java/com/gmatieso/tags/model/Task.java
+++ src/main/java/com/gmatieso/tags/model/Task.java
@@ -1,4 +1,32 @@
 package com.gmatieso.tags.model;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private boolean status = false;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "task_tag",
+            joinColumns = @JoinColumn(name = "task_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags = new HashSet<>();
+
 }
