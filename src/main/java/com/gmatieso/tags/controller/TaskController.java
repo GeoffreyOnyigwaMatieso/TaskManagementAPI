e505df76492e72963c9bfd1fb5827ab0538d2a2d
Gmatieso <matiesogeoffrey@gmail.com>
2024-11-27 02:17:06 +0300

    TaskController done

--- src/main/java/com/gmatieso/tags/controller/TaskController.java
+++ src/main/java/com/gmatieso/tags/controller/TaskController.java
@@ -1,4 +1,40 @@
 package com.gmatieso.tags.controller;
 
+
+import com.gmatieso.tags.model.Task;
+import com.gmatieso.tags.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task){
+        return  ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTask(task));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTasks() {
+        return  ResponseEntity.ok(taskService.getAllTasks());
+    }
+
+    @PutMapping("/{id}")
+    public  ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        return ResponseEntity.ok(taskService.updateTask(id, taskDetails));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
 }
