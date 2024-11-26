439e4e559c9fa043ac5e4075b5257778d5fd1bc0
Gmatieso <matiesogeoffrey@gmail.com>
2024-11-27 01:55:02 +0300

    exceptional error handling

--- src/main/java/com/gmatieso/tags/exception/ResourceNotFoundException.java
+++ src/main/java/com/gmatieso/tags/exception/ResourceNotFoundException.java
@@ -1,21 +1,7 @@
-package com.example.taskmanagement.exception;
+package com.gmatieso.tags.exception;
 
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-@ControllerAdvice
-class GlobalExceptionHandler {
-
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGlobalException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred: " + ex.getMessage());
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
     }
 }
