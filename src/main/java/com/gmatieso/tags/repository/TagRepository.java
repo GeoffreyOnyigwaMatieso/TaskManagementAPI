8ce6860ac563ff82c7c00e912b2cdb1e9eabc8d6
Gmatieso <matiesogeoffrey@gmail.com>
2024-11-26 16:28:15 +0300

    TagRepository done

--- src/main/java/com/gmatieso/tags/repository/TagRepository.java
+++ src/main/java/com/gmatieso/tags/repository/TagRepository.java
@@ -1,4 +1,13 @@
 package com.gmatieso.tags.repository;
 
-public interface TagRepository {
+
+import com.gmatieso.tags.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag,Long> {
+    Optional<Tag> findByName(String name);
 }
