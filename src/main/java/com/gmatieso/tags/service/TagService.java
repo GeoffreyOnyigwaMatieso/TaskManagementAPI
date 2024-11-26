21c862e9d70888dd801d6b70507ac02625d9c14d
Gmatieso <matiesogeoffrey@gmail.com>
2024-11-26 16:57:26 +0300

    Tag Service done

--- src/main/java/com/gmatieso/tags/service/TagService.java
+++ src/main/java/com/gmatieso/tags/service/TagService.java
@@ -1,4 +1,26 @@
 package com.gmatieso.tags.service;
 
+
+import com.gmatieso.tags.model.Tag;
+import com.gmatieso.tags.repository.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TagService {
+    @Autowired
+    private TagRepository tagRepository;
+
+
+    public List<Tag> getAllTags() {
+        return tagRepository.findAll();
+    }
+
+    public Tag getTagDetails(Long id) {
+        return tagRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Tag not found with id: " + id));
+    }
+
 }
