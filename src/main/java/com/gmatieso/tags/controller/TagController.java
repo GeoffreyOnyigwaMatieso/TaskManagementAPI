49f785c8a0d820162c960a7b70ebb130a50b345f
Gmatieso <matiesogeoffrey@gmail.com>
2024-11-27 02:22:51 +0300

    Tag controller done

--- src/main/java/com/gmatieso/tags/controller/TagController.java
+++ src/main/java/com/gmatieso/tags/controller/TagController.java
@@ -1,4 +1,32 @@
 package com.gmatieso.tags.controller;
 
+
+import com.gmatieso.tags.model.Tag;
+import com.gmatieso.tags.model.Task;
+import com.gmatieso.tags.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tags")
 public class TagController {
+
+    @Autowired
+    private TagService tagService;
+
+    @GetMapping
+    private ResponseEntity<List<Tag>> getAllTags(){
+        return ResponseEntity.ok(tagService.getAllTags());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tag> getTagDetails(@PathVariable Long id) {
+        return ResponseEntity.ok(tagService.getTagDetails(id));
+    }
 }
