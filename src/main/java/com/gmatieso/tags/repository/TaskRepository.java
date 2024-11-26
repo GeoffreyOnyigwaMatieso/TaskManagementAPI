6ab7d8bac757d20a2f6c7dd5a6b153d723aee56f
Gmatieso <matiesogeoffrey@gmail.com>
2024-11-26 16:22:47 +0300

    TaskRepository Done

--- src/main/java/com/gmatieso/tags/repository/TaskRepository.java
+++ src/main/java/com/gmatieso/tags/repository/TaskRepository.java
@@ -1,4 +1,13 @@
 package com.gmatieso.tags.repository;
 
-public interface TaskRepository {
+import com.gmatieso.tags.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByStatus(boolean status);
 }
