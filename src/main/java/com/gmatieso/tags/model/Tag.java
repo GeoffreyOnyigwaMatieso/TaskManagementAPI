c24f50f47971c7cebde5b761587a9a5c296cf26b
Gmatieso <matiesogeoffrey@gmail.com>
2024-11-26 16:07:55 +0300

    TagModel done

--- src/main/java/com/gmatieso/tags/model/Tag.java
+++ src/main/java/com/gmatieso/tags/model/Tag.java
@@ -1,4 +1,24 @@
 package com.gmatieso.tags.model;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "tags", fetch = FetchType.LAZY)
+    private Set<Task> tasks = new HashSet<>();
 }
