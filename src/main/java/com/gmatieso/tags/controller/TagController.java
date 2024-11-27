1aafb14e9e3acfeec9ef9fb1ffe5d4d1037c8862
Gmatieso <matiesogeoffrey@gmail.com>
2024-11-27 21:37:02 +0300

    code refactor controller

--- src/main/java/com/gmatieso/tags/controller/TagController.java
+++ src/main/java/com/gmatieso/tags/controller/TagController.java
@@ -1,32 +1,20 @@
 package com.gmatieso.tags.controller;
 
-
-import com.gmatieso.tags.model.Tag;
-import com.gmatieso.tags.model.Task;
+import com.gmatieso.tags.dto.TagDTO;
 import com.gmatieso.tags.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/tags")
+@RequestMapping("/api/tags")
 public class TagController {
 
     @Autowired
     private TagService tagService;
 
-    @GetMapping
-    private ResponseEntity<List<Tag>> getAllTags(){
-        return ResponseEntity.ok(tagService.getAllTags());
-    }
-
     @GetMapping("/{id}")
-    public ResponseEntity<Tag> getTagDetails(@PathVariable Long id) {
-        return ResponseEntity.ok(tagService.getTagDetails(id));
+    public ResponseEntity<TagDTO> getTagById(@PathVariable Long id) {
+        return ResponseEntity.ok(tagService.getTagById(id));
     }
 }
--- src/main/java/com/gmatieso/tags/controller/TaskController.java
+++ src/main/java/com/gmatieso/tags/controller/TaskController.java
@@ -1,40 +1,30 @@
 package com.gmatieso.tags.controller;
 
-
-import com.gmatieso.tags.model.Task;
+import com.gmatieso.tags.dto.TaskDTO;
 import com.gmatieso.tags.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/tasks")
+@RequestMapping("/api/tasks")
 public class TaskController {
 
     @Autowired
     private TaskService taskService;
 
-    @PostMapping
-    public ResponseEntity<Task> createTask(@RequestBody Task task){
-        return  ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTask(task));
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskDTO> getTaskById(@PathVariable Long id) {
+        return ResponseEntity.ok(taskService.getTaskById(id));
     }
 
-    @GetMapping
-    public ResponseEntity<List<Task>> getAllTasks() {
-        return  ResponseEntity.ok(taskService.getAllTasks());
+    @PostMapping
+    public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO taskDTO) {
+        return ResponseEntity.ok(taskService.createTask(taskDTO));
     }
 
     @PutMapping("/{id}")
-    public  ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
-        return ResponseEntity.ok(taskService.updateTask(id, taskDetails));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
-        taskService.deleteTask(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<TaskDTO> updateTask(@PathVariable Long id, @RequestBody TaskDTO taskDTO) {
+        return ResponseEntity.ok(taskService.updateTask(id, taskDTO));
     }
 }
