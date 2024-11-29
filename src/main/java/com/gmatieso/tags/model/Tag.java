ca82f20cb52e6db5e748482240193c1d83583a71
Gmatieso <matiesogeoffrey@gmail.com>
2024-11-29 12:55:59 +0300

    models done

--- src/main/java/com/gmatieso/tags/model/Tag.java
+++ src/main/java/com/gmatieso/tags/model/Tag.java
@@ -1,4 +1,24 @@
 package com.gmatieso.tags.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Task> tasks;
 }
--- src/main/java/com/gmatieso/tags/model/Task.java
+++ src/main/java/com/gmatieso/tags/model/Task.java
@@ -1,4 +1,31 @@
 package com.gmatieso.tags.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private boolean completed;
+
+    @ManyToMany
+    @JoinTable(
+            name = "task_tag",
+            joinColumns = @JoinColumn(name = "task_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags;
 }
