3e62cc5d9eeb1a7e7d0c8c7b7c23a22c62644c28
Gmatieso <matiesogeoffrey@gmail.com>
2024-11-29 13:00:20 +0300

    DTO done

---
+++ src/main/java/com/gmatieso/tags/dto/TagDTO.java
@@ -0,0 +1,12 @@
+package com.gmatieso.tags.dto;
+
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class TagDTO {
+    private Long id;
+    private String name;
+    private Set<String> tasks;
+}
---
+++ src/main/java/com/gmatieso/tags/dto/TaskDTO.java
@@ -0,0 +1,13 @@
+package com.gmatieso.tags.dto;
+
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class TaskDTO {
+    private Long id;
+    private String title;
+    private boolean completed;
+    private Set<String> tags;
+}
