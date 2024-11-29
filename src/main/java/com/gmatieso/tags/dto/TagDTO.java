a0ad04dec373d148de2311f426149a8f90e39af5
Gmatieso <matiesogeoffrey@gmail.com>
2024-11-29 12:41:02 +0300

    solving  No suitable HttpMessageConverter

--- src/main/java/com/gmatieso/tags/dto/TagDTO.java
+++ src/main/java/com/gmatieso/tags/dto/TagDTO.java
@@ -1,9 +1,11 @@
 package com.gmatieso.tags.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class TagDTO {
+    @JsonProperty("tag_id")
     private Long id;
     private String name;
 }
--- src/main/java/com/gmatieso/tags/dto/TaskDTO.java
+++ src/main/java/com/gmatieso/tags/dto/TaskDTO.java
@@ -1,12 +1,20 @@
 package com.gmatieso.tags.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import java.util.Set;
 
 @Data
 public class TaskDTO {
+    @JsonProperty("task_id")
     private Long id;
+
     private String title;
+
     private boolean status;
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private Set<String> tags;
 }
+
+
