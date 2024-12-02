a598ed8bfa3a191377f30fd0b8f077b575faf413
Gmatieso <matiesogeoffrey@gmail.com>
2024-12-02 07:49:08 +0300

    refactoring our Task model and controller

--- src/main/java/com/gmatieso/tags/model/Task.java
+++ src/main/java/com/gmatieso/tags/model/Task.java
@@ -1,6 +1,7 @@
 package com.gmatieso.tags.model;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,20 +13,25 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "\"Task\"")
+@Schema(description = "Task Entity")
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Unique identifier of task")
     private Long id;
 
+    @Schema(description = "title")
     private String title;
 
+    @Schema(description = "status")
     private boolean completed;
 
-    @ManyToMany
-    @JoinTable(
-            name = "task_tag",
-            joinColumns = @JoinColumn(name = "task_id"),
-            inverseJoinColumns = @JoinColumn(name = "tag_id")
-    )
-    private Set<Tag> tags;
+//    @ManyToMany
+//    @JoinTable(
+//            name = "task_tag",
+//            joinColumns = @JoinColumn(name = "task_id"),
+//            inverseJoinColumns = @JoinColumn(name = "tag_id")
+//    )
+//    private Set<Tag> tags;
 }
