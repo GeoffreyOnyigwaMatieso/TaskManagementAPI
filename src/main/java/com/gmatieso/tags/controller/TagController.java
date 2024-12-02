02b5955fc9c9b52e2c411cceb55f476333809076
Gmatieso <matiesogeoffrey@gmail.com>
2024-12-02 05:37:39 +0300

    Add TagController implementation with endpoints for retrieving all tags and tag details by ID

--- src/main/java/com/gmatieso/tags/controller/TagController.java
+++ src/main/java/com/gmatieso/tags/controller/TagController.java
@@ -1,4 +1,31 @@
 package com.gmatieso.tags.controller;
 
+import com.gmatieso.tags.dto.TagDTO;
+import com.gmatieso.tags.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/tags")
 public class TagController {
+
+    @Autowired
+    private TagService tagService;
+
+    // Endpoint to get all tags
+    @GetMapping
+    public ResponseEntity<List<TagDTO>> getAllTags() {
+        List<TagDTO> tags = tagService.getAllTags();
+        return ResponseEntity.ok(tags);
+    }
+
+    // Endpoint to get details of a single tag by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<TagDTO> getTagDetails(@PathVariable Long id) {
+        TagDTO tagDetails = tagService.getTagDetails(id);
+        return ResponseEntity.ok(tagDetails);
+    }
 }
