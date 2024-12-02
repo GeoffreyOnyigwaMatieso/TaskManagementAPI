cfe6929eca24aa68759d77eb205bc3ab2e4a92ab
Gmatieso <matiesogeoffrey@gmail.com>
2024-12-02 07:52:50 +0300

    Tag service done

--- src/main/java/com/gmatieso/tags/service/TagService.java
+++ src/main/java/com/gmatieso/tags/service/TagService.java
@@ -12,23 +12,23 @@ import java.util.stream.Collectors;
 @Service
 public class TagService {
 
-    @Autowired
-    private TagRepository tagRepository;
-
-    public List<TagDTO> getAllTags() {
-        return tagRepository.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
-    }
-
-    public TagDTO getTagDetails(Long id) {
-        Tag tag = tagRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Tag not found with id: " + id));
-        return mapToDTO(tag);
-    }
-
-    private TagDTO mapToDTO(Tag tag) {
-        TagDTO dto = new TagDTO();
-        dto.setId(tag.getId());
-        dto.setName(tag.getName());
-        dto.setTasks(tag.getTasks().stream().map(task -> task.getTitle()).collect(Collectors.toSet()));
-        return dto;
-    }
+//    @Autowired
+//    private TagRepository tagRepository;
+//
+//    public List<TagDTO> getAllTags() {
+//        return tagRepository.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+//    }
+//
+//    public TagDTO getTagDetails(Integer id) {
+//        Tag tag = tagRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Tag not found with id: " + id));
+//        return mapToDTO(tag);
+//    }
+//
+//    private TagDTO mapToDTO(Tag tag) {
+//        TagDTO dto = new TagDTO();
+//        dto.setId(tag.getId());
+//        dto.setName(tag.getName());
+//        dto.setTasks(tag.getTasks().stream().map(task -> task.getTitle()).collect(Collectors.toSet()));
+//        return dto;
+//    }
 }
